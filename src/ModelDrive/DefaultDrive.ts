54ccc6393bd5bfb1cf151f5492a7e20e22ec5cd6
zwa73 <ssfen2419@126.com>
2025-09-29 12:56:52 +0800

    fx

--- src/ModelDrive/DefaultDrive.ts
+++ src/ModelDrive/DefaultDrive.ts
@@ -6,11 +6,11 @@ import { ChatTaskInterface, DefChatLaMResult, TaskInterface } from "Task";
 export class DefaultDrive implements TaskInterface{
     chat:ChatTaskInterface = {
         async execute(){
-            SLogger.warn(`DefaultDrive.chat.execute 未实现`);
+            SLogger.warn(`DefaultDrive.chat.execute 被调用, 可能是某个Drive未实现task接口, 或是实例不存在`);
             return DefChatLaMResult;
         },
         async countToken(){
-            SLogger.warn(`DefaultDrive.chat.countToken 未实现`);
+            SLogger.warn(`DefaultDrive.chat.countToken 被调用, 可能是某个Drive未实现task接口, 或是实例不存在`);
             return 0
         }
     };
