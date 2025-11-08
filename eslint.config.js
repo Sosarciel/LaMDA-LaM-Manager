bf05da9e9f642b950f67b276309a4dd2e221336d
zwa73 <ssfen2419@126.com>
2025-11-08 09:26:12 +0800

    add lint

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const {parser:tsparser,plugin:tsplugin} = require('typescript-eslint');
+const globals = require("globals");
+
+module.exports = [
+    {
+        ignores: ["*.js", "dist/**", "backup/**", "scripts/**"],
+        plugins: {
+            "@typescript-eslint": tsplugin
+        },
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            parser:tsparser,
+            parserOptions: {
+                project: "tsconfig.json",
+                tsconfigRootDir: __dirname
+            },
+            globals: {
+                ...globals.node,
+                ...globals.commonjs,
+            },
+        },
+    },
+    {
+        files: ['src/**/*.ts'],
+        ignores: ['src/test/**'],
+        rules: {
+            semi: ['error', 'always'],
+            '@typescript-eslint/promise-function-async': 'error',
+            '@typescript-eslint/no-floating-promises': 'error',
+        },
+    }
+];
