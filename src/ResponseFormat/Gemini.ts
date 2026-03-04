1f1c4468d093328a12eedd5a59b731f22838eab0
zwa73 <ssfen2419@126.com>
2026-03-05 04:21:02 +0800

    fx gmn

--- src/ResponseFormat/Gemini.ts
+++ src/ResponseFormat/Gemini.ts
@@ -1,42 +1,6 @@
 
 
 
-const GeminiResponseExample = {
-    candidates: [
-        {
-            content: {
-                parts: [
-                    {
-                        text: '你好！ 你好吗？ (Nǐ hǎo! Nǐ hǎo ma?)  \n \nThis means "Hello! How are you?"  How can I help you today?\n',
-                    },
-                ],
-                role: "model",
-            },
-            finishReason: "STOP",
-            avgLogprobs: -0.20637991370224371,
-        },
-    ],
-    usageMetadata: {
-        promptTokenCount: 5,
-        candidatesTokenCount: 41,
-        totalTokenCount: 46,
-        promptTokensDetails: [
-            {
-                modality: "TEXT",
-                tokenCount: 5,
-            },
-        ],
-        candidatesTokensDetails: [
-            {
-                modality: "TEXT",
-                tokenCount: 41,
-            },
-        ],
-    },
-    modelVersion: "gemini-1.5-flash",
-};
-
-
 
 
 
@@ -130,3 +94,41 @@ export type GeminiResponse = {
     /** 模型版本 */
     modelVersion:string,
 }
+
+
+
+export const GeminiResponseExample = {
+    candidates: [
+        {
+            content: {
+                parts: [
+                    {
+                        text: '你好！ 你好吗？ (Nǐ hǎo! Nǐ hǎo ma?)  \n \nThis means "Hello! How are you?"  How can I help you today?\n',
+                    },
+                ],
+                role: "model",
+            },
+            finishReason: "STOP",
+            avgLogprobs: -0.20637991370224371,
+        },
+    ],
+    usageMetadata: {
+        thoughtsTokenCount: 0,
+        promptTokenCount: 5,
+        candidatesTokenCount: 41,
+        totalTokenCount: 46,
+        promptTokensDetails: [
+            {
+                modality: "TEXT",
+                tokenCount: 5,
+            },
+        ],
+        candidatesTokensDetails: [
+            {
+                modality: "TEXT",
+                tokenCount: 41,
+            },
+        ],
+    },
+    modelVersion: "gemini-1.5-flash",
+} as const satisfies GeminiResponse;
