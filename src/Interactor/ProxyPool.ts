a91a4d2f31d40cd3db47d6bc2f2aaf48f5baf860
zwa73 <ssfen2419@126.com>
2026-03-23 22:37:29 +0800

    fx

--- src/Interactor/ProxyPool.ts
+++ src/Interactor/ProxyPool.ts
@@ -1,20 +1,18 @@
 
 import type { PRecord } from '@zwa73/utils';
 import { match } from '@zwa73/utils';
-import type { HttpProxyAgent } from 'http-proxy-agent';
-import createHttpProxyAgent from 'http-proxy-agent';
-import type { HttpsProxyAgent } from 'https-proxy-agent';
-import createHttpsProxyAgent from 'https-proxy-agent';
+import { HttpProxyAgent } from 'http-proxy-agent';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 
 
 const ProxyPool = {
-    http:{} as PRecord<string, HttpProxyAgent>,
-    https:{} as PRecord<string, HttpsProxyAgent>
+    http:{} as PRecord<string, HttpProxyAgent<string>>,
+    https:{} as PRecord<string, HttpsProxyAgent<string>>
 };
 export const getProxy = (type:'http'|'https',url:string)=>{
     return match(type,{
-        http:()=>ProxyPool.http[url] ?? (ProxyPool.http[url] = createHttpProxyAgent(url)),
-        https:()=>ProxyPool.https[url] ?? (ProxyPool.https[url] = createHttpsProxyAgent(url)),
+        http:()=>ProxyPool.http[url] ?? (ProxyPool.http[url] = new HttpProxyAgent(url)),
+        https:()=>ProxyPool.https[url] ?? (ProxyPool.https[url] = new HttpsProxyAgent(url)),
     });
 };
